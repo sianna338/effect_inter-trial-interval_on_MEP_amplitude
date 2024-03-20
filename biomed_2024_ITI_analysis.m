4b652d39e44f996e0355ff8474cfc291a41ebaed
sianna338 <157957826+sianna338@users.noreply.github.com>
2024-03-20 18:20:06 +0100

    Update biomed_2024_ITI_analysis.m

--- biomed_2024_ITI_analysis.m
+++ biomed_2024_ITI_analysis.m
@@ -1,293 +1,91 @@
-%% fieldtrip
+%% preliminaries 
 addpath('C:\Users\siann\Downloads\fieldtrip-20231113\fieldtrip-20231113')
 ft_defaults
 
-% look at markers in dataset
-cfg = [];
-cfg.dataset             = 'biomed2024_sub03_ses1_SP.eeg';
-cfg.trialdef.eventtype = '?';
-dummy                   = ft_definetrial(cfg);
+datapath = 'C:\Users\siann\Data\biomed_2024\';
+% subjects = {'sub01', 'sub02', 'sub03', 'sub04', 'sub05'};
+subjects = {'sub03'};
+sessions = {'ses1', 'ses2'};
+conditions = {'SP', 'SICI', 'SICF'};
 
-%% create trial stucture 
-cfg = [];
-cfg.datafile = 'biomed2024_sub03_ses1_SP.eeg';
-cfg.continous = 'yes';
-cfg.channel = {'FDIr'};
-cfg.trialdef.prestim = 0.05; 
-cfg.trialdef.poststim = 0.1; 
-cfg.trialdef.eventtype = 'Out';
-cfg.trialdef.eventvalue = 'B';
-cfg = ft_definetrial(cfg);
-trial_matrix = cfg.trl
-% read-in data
-% cfg = []; 
-cfg.datafile = 'biomed2024_sub03_ses1_SP.eeg';
-cfg.headerfile = 'biomed2024_sub03_ses1_SP.vhdr';
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-cfg.demean='yes';
-cfg.baselinewindow = [-0.05 -0.03];
-data_raw_sp_01= ft_preprocessing(cfg);
-save('data_raw_sp_01','data_raw_sp_01','-v7.3');
 
-%% create trial stucture 
-cfg = [];
-cfg.datafile = 'biomed2024_sub03_ses1_SP.eeg';
-cfg.continous = 'yes';
-cfg.channel = {'FDIr'};
-cfg.trialdef.prestim = 0.05; 
-cfg.trialdef.poststim = 0.1; 
-cfg.trialdef.eventtype = 'Out';
-cfg.trialdef.eventvalue = 'B';
-cfg = ft_definetrial(cfg);
+%% main analysis: loop over subjects
+for subi = 1:numel(subjects)
+    for sesi = 1:numel(sessions)
+        for condi = 1:numel(conditions )
+            % look at markers in dataset and segment based on markers
+            cfg = [];
+            cfg.datafile = strcat(datapath, subjects{subi}, filesep, 'biomed2024', '_', subjects{subi}, '_', sessions{sesi}, '_', conditions{condi}, '.eeg')
+            cfg.continous = 'yes';
+            cfg.channel = 'FDIr';
+            cfg.trialdef.prestim = 0.05; 
+            cfg.trialdef.poststim = 0.1; 
+            cfg.trialdef.eventtype = 'Out';
+            cfg.trialdef.eventvalue = 'B';
+            cfg = ft_definetrial(cfg);
+            trial_matrix = cfg.trl
+            % read-in data
+            % cfg = []; 
+            cfg.headerfile = strcat(datapath, 'biomed2024_', subjects{subi}, '_', sessions{sesi}, '_', conditions{condi}, '.vhdr');
+            cfg.channel = 'FDIr'; % indicate the channels we would like to read and/or exclude.
+            data_raw = ft_preprocessing(cfg);
+            save(strcat(datapath,subjects{subi}, filesep,'data_raw_', subjects{subi}, '_', sessions{sesi}, '_', conditions{condi}), '-v7.3');
 
-% read-in data
-% cfg = []; 
-cfg.datafile = 'biomed2024_sub03_ses2_SP.eeg';
-cfg.headerfile = 'biomed2024_sub03_ses2_SP.vhdr';
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-cfg.demean='yes';
-cfg.baselinewindow = [-0.05 -0.03];
-data_raw_sp_02= ft_preprocessing(cfg);
-save('data_raw_sp_02','data_raw_sp_02','-v7.3');
 
-%% create trial stucture 
-cfg = [];
-cfg.datafile = 'biomed2024_sub03_ses1_SICI.eeg';
-cfg.continous = 'yes';
-cfg.channel = {'FDIr'};
-cfg.trialdef.prestim = 0.05; 
-cfg.trialdef.poststim = 0.1; 
-cfg.trialdef.eventtype = 'Out';
-cfg.trialdef.eventvalue = 'B';
-cfg = ft_definetrial(cfg);
+            % extract min and max points for for the time window from 0.015-0.05 after
+            % TMS pulse
+            cfg=[];
+            cfg.latency=[0.015 0.05];
+            cfg.channel = {'FDIr'};
+            data_MEPs = ft_selectdata(cfg, data_raw);
+            for i = 1:numel(data_MEPs.trial)
+                min_val = min(data_MEPs.trial{i}(1,:));
+                max_val = max(data_MEPs.trial{i}(1,:));
+                data_MEPs.mep(i,1) = abs(min_val) + abs(max_val);
+            end
+            save(strcat(datapath, subjects{subi}, filesep, 'data_raw_', subjects{subi}, '_', sessions{sesi}, '_', conditions{condi}, '_data_MEPs'), '-v7.3');
+        end
+    end 
+end 
+%% get ITIs and MEPs for each condition (concatenate sessions together)
+% Initialize variables
+data_all = [];
+conditions_all = [];
+data_MEPs_stored = []
+for subi = 1:numel(subjects)
+    for condi = 1:numel(conditions)
+        for sesi = 1:numel(sessions)
+            % Load data
+            load(strcat(datapath, subjects{subi}, filesep,'data_raw_', subjects{subi}, '_', sessions{sesi}, '_', conditions{condi}, '_', 'data_MEPs.mat'));
+            data_all = [data_all; data_MEPs.mep];
+            % Get ITIs on each trial
+            load(strcat(datapath, subjects{subi}, filesep, 'BEST_toolbox', filesep, subjects{subi}, filesep, 'Experiment', filesep, 'BESTData_biomed_', subjects{subi}, '_', sessions{sesi}, '_', conditions{condi}));
+            conditions_all = [conditions_all; cell2mat(BESTData.trialinfomatrix(:,4))];
+            data_MEPs_stored = [data_MEPs_stored; BESTData.Results.FDIr.MEPAmplitude]
+        end
+        data = [conditions_all, data_all, data_MEPs_stored];
+        save(strcat(datapath, subjects{subi}, filesep, 'data_', subjects{subi}, '_', conditions{condi}, '_data_MEPs_ITI'), '-v7.3');
+    end
+end 
+    %% Count how many times each ITI occurs and then get mean MEP amplitude for each ITI
+    ITI_counts = [];
+    average_MEP = [];
+     for subi = 1:length(subjects)
+        figure
+        for condi = 1:length(conditions)
+            load(strcat(datapath, subjects{subi}, filesep, 'data_', subjects{subi}, '_', conditions{condi}, '_data_MEPs_ITI'));
+            [unique_ITI, ~, idx] = unique(data(:,1));
+            ITI_counts = accumarray(idx, 1);
+            average_MEP = accumarray(idx, data(:,2), [], @mean);
+            average_MEP_stored = accumarray(idx, data(:,3), [], @mean)
+            MEP_avg = struct('ITI', unique_ITI, 'Count', ITI_counts, 'Average_MEP', average_MEP, 'Average_MEP_stored', average_MEP_stored, 'diff', abs(average_MEP_stored-average_MEP));
+            save(strcat(datapath, subjects{subi}, filesep, 'data_', subjects{subi}, '_', conditions{condi}, '_data_average_MEPs_ITI'), '-v7.3');
 
-% read-in data
-% cfg = []; 
-cfg.datafile = 'biomed2024_sub03_ses1_SICI.eeg';
-cfg.headerfile = 'biomed2024_sub03_ses1_SICI.vhdr';
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-cfg.demean='yes';
-cfg.baselinewindow = [-0.05 -0.03];
-data_raw_sici_01= ft_preprocessing(cfg);
-save('data_raw_sici_01','data_raw_sici_01','-v7.3');
-
-%% create trial stucture 
-cfg = [];
-cfg.datafile = 'biomed2024_sub03_ses2_SICI.eeg';
-cfg.continous = 'yes';
-cfg.channel = {'FDIr'};
-cfg.trialdef.prestim = 0.05; 
-cfg.trialdef.poststim = 0.1; 
-cfg.trialdef.eventtype = 'Out';
-cfg.trialdef.eventvalue = 'B';
-cfg = ft_definetrial(cfg);
-
-% read-in data
-% cfg = []; 
-cfg.datafile = 'biomed2024_sub03_ses2_SICI.eeg';
-cfg.headerfile = 'biomed2024_sub03_ses2_SICI.vhdr';
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-cfg.demean='yes';
-cfg.baselinewindow = [-0.05 -0.03];
-data_raw_sici_02= ft_preprocessing(cfg);
-save('data_raw_sici_02','data_raw_sici_02','-v7.3');
-
-%% create trial stucture 
-cfg = [];
-cfg.datafile = 'biomed2024_sub03_ses1_SICF.eeg';
-cfg.continous = 'yes';
-cfg.channel = {'FDIr'};
-cfg.trialdef.prestim = 0.05; 
-cfg.trialdef.poststim = 0.1; 
-cfg.trialdef.eventtype = 'Out';
-cfg.trialdef.eventvalue = 'B';
-cfg = ft_definetrial(cfg);
-
-% read-in data
-% cfg = []; 
-cfg.datafile = 'biomed2024_sub03_ses1_SICF.eeg';
-cfg.headerfile = 'biomed2024_sub03_ses1_SICF.vhdr';
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-cfg.demean='yes';
-cfg.baselinewindow = [-0.05 -0.03];
-data_raw_sicf_01= ft_preprocessing(cfg);
-save('data_raw_sicf_01','data_raw_sicf_01','-v7.3');
-
-%% create trial stucture 
-cfg = [];
-cfg.datafile = 'biomed2024_sub03_ses2_SICF.eeg';
-cfg.continous = 'yes';
-cfg.channel = {'FDIr'};
-cfg.trialdef.prestim = 0.05; 
-cfg.trialdef.poststim = 0.1; 
-cfg.trialdef.eventtype = 'Out';
-cfg.trialdef.eventvalue = 'B';
-cfg = ft_definetrial(cfg);
-
-% read-in data
-% cfg = []; 
-cfg.datafile = 'biomed2024_sub03_ses2_SICF.eeg';
-cfg.headerfile = 'biomed2024_sub03_ses2_SICF.vhdr';
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-cfg.demean='yes';
-cfg.baselinewindow = [-0.05 -0.03];
-data_raw_sicf_02= ft_preprocessing(cfg);
-save('data_raw_sicf_02','data_raw_sicf_02','-v7.3');
-
-%% extract min and max points for for the time window from 0.015-0.05 after
-%% TMS pulse
-cfg=[];
-cfg.latency=[0.015 0.05];
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-% cfg.trials=find(data.trialinfo==B); 
-data_MEPs_sp_01=ft_selectdata(cfg,data_raw_sp_01);
-for i=1:size(data_MEPs_sp_01.trial,2) % for each trial (dimension 2)
-    min_val=min(data_MEPs_sp_01.trial{1,i}(1,:)); % only one channel (FDI), look through all timepoints in selected window
-    max_val=max(data_MEPs_sp_01.trial{1,i}(1,:));
-    data_MEPs_sp_01.mep(i,1)=abs(min_val)+abs(max_val); % create list of MEP amplitudes with new row for each trial
-end
-% data_MEPs_sp_01.mep(81:100,:) = zeros(20,1); % not enough trials here, battery died
-
-%% extract min and max points for for the time window from 0.015-0.05 after TMS pulse
-cfg=[];
-cfg.latency=[0.015 0.05];
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-% cfg.trials=find(data.trialinfo==B); 
-data_MEPs_sp_02=ft_selectdata(cfg,data_raw_sp_02);
-for i=1:size(data_MEPs_sp_02.trial,2) % for each trial (dimension 2)
-    min_val=min(data_MEPs_sp_02.trial{1,i}(1,:)); % only one channel (APBr), look through all timepoints in selected window
-    max_val=max(data_MEPs_sp_02.trial{1,i}(1,:));
-    data_MEPs_sp_02.mep(i,1)=abs(min_val)+abs(max_val); % create list of MEP amplitudes with new row for each trial
-end
-
-%% extract min and max points for for the time window from 0.015-0.05 after
-% TMS pulse
-cfg=[];
-cfg.latency=[0.015 0.05];
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-% cfg.trials=find(data.trialinfo==B); 
-data_MEPs_sici_01=ft_selectdata(cfg,data_raw_sici_01);
-for i=1:size(data_MEPs_sici_01.trial,2) % for each trial (dimension 2)
-    min_val=min(data_MEPs_sici_01.trial{1,i}(1,:)); % only one channel (APBr), look through all timepoints in selected window
-    max_val=max(data_MEPs_sici_01.trial{1,i}(1,:));
-    data_MEPs_sici_01.mep(i,1)=abs(min_val)+abs(max_val); % create list of MEP amplitudes with new row for each trial
-end
-
-%% extract min and max points for for the time window from 0.015-0.05 after
-% TMS pulse
-cfg=[];
-cfg.latency=[0.015 0.05];
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-% cfg.trials=find(data.trialinfo==B); 
-data_MEPs_sici_02=ft_selectdata(cfg,data_raw_sici_02);
-for i=1:size(data_MEPs_sici_02.trial,2) % for each trial (dimension 2)
-    min_val=min(data_MEPs_sici_02.trial{1,i}(1,:)); % only one channel (APBr), look through all timepoints in selected window
-    max_val=max(data_MEPs_sici_02.trial{1,i}(1,:));
-    data_MEPs_sici_02.mep(i,1)=abs(min_val)+abs(max_val); % create list of MEP amplitudes with new row for each trial
-end
-
-%% extract min and max points for for the time window from 0.015-0.05 after
-% TMS pulse
-cfg=[];
-cfg.latency=[0.015 0.05];
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-% cfg.trials=find(data.trialinfo==B); 
-data_MEPs_sicf_01=ft_selectdata(cfg,data_raw_sicf_01);
-for i=1:size(data_MEPs_sicf_01.trial,2) % for each trial (dimension 2)
-    min_val=min(data_MEPs_sicf_01.trial{1,i}(1,:)); % only one channel (APBr), look through all timepoints in selected window
-    max_val=max(data_MEPs_sicf_01.trial{1,i}(1,:));
-    data_MEPs_sicf_01.mep(i,1)=abs(min_val)+abs(max_val); % create list of MEP amplitudes with new row for each trial
-end
-
-%% extract min and max points for for the time window from 0.015-0.05 after
-% TMS pulse
-cfg=[];
-cfg.latency=[0.015 0.05];
-cfg.channel = {'FDIr'}; % indicate the channels we would like to read and/or exclude.
-% cfg.trials=find(data.trialinfo==B); 
-data_MEPs_sicf_02=ft_selectdata(cfg,data_raw_sicf_02);
-for i=1:size(data_MEPs_sicf_02.trial,2) % for each trial (dimension 2)
-    min_val=min(data_MEPs_sicf_02.trial{1,i}(1,:)); % only one channel (APBr), look through all timepoints in selected window
-    max_val=max(data_MEPs_sicf_02.trial{1,i}(1,:));
-    data_MEPs_sicf_02.mep(i,1)=abs(min_val)+abs(max_val); % create list of MEP amplitudes with new row for each trial
-end
-%% get conditions on each trial
-conditions_SP = cell2mat(vertcat(SP_01.trialinfomatrix(:, 4), SP_02.trialinfomatrix(:, 4))); % for some reason SP_ses02 missing
-conditions_SICI = cell2mat(vertcat(pp_SICI_01.trialinfomatrix(:, 4), pp_SICI_02.trialinfomatrix(:, 4)));
-conditions_SICF = cell2mat(vertcat(pp_SICF_01.trialinfomatrix(:, 4), pp_SICF_02.trialinfomatrix(:, 4)));
-
-%% get MEP amplitude for each condition
-data_SP = zeros(200,2); 
-data_SP(: ,1) = conditions_SP; 
-data_SP(:,2) = vertcat(data_MEPs_sp_01.mep, data_MEPs_sp_02.mep); 
-
-data_SICI = zeros(200,2);
-data_SICI(:, 1) = conditions_SICI;
-data_SICI(:, 2) = vertcat(data_MEPs_sici_01.mep, data_MEPs_sici_02.mep); 
-
-data_SICF = zeros(200,2);
-data_SICF(:, 1) = conditions_SICF; 
-data_SICF(:, 2) = vertcat(data_MEPs_sicf_01.mep, data_MEPs_sicf_02.mep);
-
-%% count how many times each ITI occurs and then get mean MEP amplitude for each ITI
-[unique_ITI, ~, idx] = unique(data_SP(:,1));
-ITI_counts = accumarray(idx, 1);
-
-% Calculate average MEP amplitudes for each unique ITI
-average_MEP_sp = accumarray(idx, data_SP(:,2), [], @mean);
-
-% Create a structure to store results
-MEP_avg_sp = struct('ITI', unique_ITI, 'Count', ITI_counts, 'Average_MEP', average_MEP_sp);
-
-%% count how many times each ITI occurs and then get mean MEP amplitude for each ITI
-[unique_ITI, ~, idx] = unique(data_SP(:,1));
-ITI_counts = accumarray(idx, 1);
-
-% Calculate average MEP amplitudes for each unique ITI
-average_MEP_sp = accumarray(idx, data_SP(:,2), [], @mean);
-
-% Create a structure to store results
-MEP_avg_sp = struct('ITI', unique_ITI, 'Count', ITI_counts, 'Average_MEP', average_MEP_sp);
-
-%% count how many times each ITI occurs and then get mean MEP amplitude for each ITI
-[unique_ITI, ~, idx] = unique(data_SICI(:,1));
-ITI_counts = accumarray(idx, 1);
-
-% Calculate average MEP amplitudes for each unique ITI
-average_MEP_sici = accumarray(idx, data_SICI(:,2), [], @mean);
-
-% Create a structure to store results
-MEP_avg_sici = struct('ITI', unique_ITI, 'Count', ITI_counts, 'Average_MEP', average_MEP_sici);
-
-%% count how many times each ITI occurs and then get mean MEP amplitude for each ITI
-[unique_ITI, ~, idx] = unique(data_SICF(:,1));
-ITI_counts = accumarray(idx, 1);
-
-% Calculate average MEP amplitudes for each unique ITI
-average_MEP_sicf = accumarray(idx, data_SICF(:,2), [], @mean);
-
-% Create a structure to store results
-MEP_avg_sicf = struct('ITI', unique_ITI, 'Count', ITI_counts, 'Average_MEP', average_MEP_sicf);
-
-%% plot
-figure; 
-plot(MEP_avg_sp.ITI, MEP_avg_sp.Average_MEP, '-ok', 'MarkerFaceColor','k')
-title('single pulse MEP amplitudes')
-xlabel('ITIs')
-ylabel('MEP amplitude')
-
-figure; 
-plot(MEP_avg_sici.ITI, MEP_avg_sici.Average_MEP, '-ok', 'MarkerFaceColor','k')
-title('SICI MEP amplitudes')
-xlabel('ITIs')
-ylabel('MEP amplitude')
-
-figure; 
-plot(MEP_avg_sicf.ITI, MEP_avg_sicf.Average_MEP, '-ok', 'MarkerFaceColor','k')
-title('SICF MEP amplitudes')
-xlabel('ITIs')
-ylabel('MEP amplitude')
-
-%% ANOVA
-% [p,tbl,stats] = anova1(data_MEPs.mep, conditions);
-
+            % Plot
+            subplot(1, numel(conditions), condi);
+            plot(MEP_avg.ITI, MEP_avg.Average_MEP, '-ok', 'MarkerFaceColor','k');
+            title([subjects{subi}, ' ', conditions{condi}, ' MEP amplitudes']);
+            xlabel('ITIs');
+            ylabel('MEP amplitude');
+        end
+    end 
